056b3f2c0ae96b735bd248350cfad8b41840759d
htl-leonding <t.stuetz@htl-leonding.ac.at>
2023-11-13 14:01:21 +0100

    lecture notes 2023-11-13

--- labs/vehicle/src/main/java/at/htlleonding/vehicle/control/VehicleRepository.java
+++ labs/vehicle/src/main/java/at/htlleonding/vehicle/control/VehicleRepository.java
@@ -1,7 +1,18 @@
 package at.htlleonding.vehicle.control;
 
+import at.htlleonding.vehicle.entity.Vehicle;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 
 @ApplicationScoped
 public class VehicleRepository {
+
+    @Inject
+    EntityManager em;
+
+    public void save(Vehicle vehicle) {
+        //em.
+    }
+
 }
